21dc19895d14281697774ab8ebce0fc1302ed661
LTMax2102 <jamie.selwyn@live.co.uk>
2022-04-23 22:27:20 +0100

    Added more features

--- chess/include/chess.h
+++ chess/include/chess.h
@@ -5,10 +5,16 @@
 #include <array>
 #include <vector>
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <map>
 class chess {
-    int count = 0;
+    int count = 0, column, letter = 1, rowInt, maxMovesY, maxMovesX, movement;
+    char selection, row;
+    std::string input;
+    std::map<char, std::string> pieces = {{'R', "Rook"}, {'H', "Horse"},{'B', "Bishop"},{'Q', "Queen"},{'K', "King"},{'P', "Pawn"}};
+    std::vector<std::string> validMoves;
     std::array<char,8> alpha = {'A','B','C','D','E','F','G','H'};
-    int letter = 1;
     char board[8][8] = {{'R','H','B','Q','K','B','H','R'},
                         {'P','P','P','P','P','P','P','P'},
                         {'-','-','-','-','-','-','-','-'},
@@ -17,11 +23,8 @@ class chess {
                         {'-','-','-','-','-','-','-','-'},
                         {'P','P','P','P','P','P','P','P'},
                         {'R','H','B','K','Q','B','H','R'}};
-    char row;
-    int column;
-    int rowInt;
-    char selection;
-public:
+
+
     void printBoard(){
         std::cout << "A: ";
         for(int i = 0; i < 8; i++){
@@ -35,6 +38,8 @@ public:
                 count += 1;
             }
         }
+        letter = 1;
+        count = 0;
         std::cout << "\n  ";
 
         for(int i = 0; i < 8; i++){
@@ -42,10 +47,108 @@ public:
         }
         std::cout << std::endl;
     }
+    bool checker(char &c){
+        for(int i = 0; i < alpha.size(); i++){
+            if(c == alpha[i]){
+                return true;
+            }
+            continue;
+        }
+        return false;
+    }
+    bool checkAndBreak(std::string &str){
+        if(str.size() > 2){std::cout << "Input must be a length of 2\n\n";return false;}
+        row = str[0];
+        column = static_cast<int>(str[1]) - 48;
+        if(checker(row) == false){
+            std::cout << "Invalid letter entered\n\n";
+            return false;
+        }
+        else if(column > 8 || column < 1){
+            std::cout << "Invalid number entered\n";
+            return false;
+        }
+        printf("Row %c Column %d Selected\n", row, column);
+        return true;
+    }
+    void getSelection(){
+            switch(row){
+        case 'A':
+            rowInt = 0;
+            break;
+        case 'B':
+            rowInt = 1;
+            break;
+        case 'C':
+            rowInt = 2;
+            break;
+        case 'D':
+            rowInt = 3;
+            break;
+        case 'E':
+            rowInt = 4;
+            break;
+        case 'F':
+            rowInt = 5;
+            break;
+        case 'G':
+            rowInt = 6;
+            break;
+        case 'H':
+            rowInt = 7;
+            break;
+        default:
+            std::cout << "Letter is not Valid\n";
+        }
+        selection = board[rowInt][column-1];
+        switch(selection){
+        case 'P':
+            maxMovesX = 0;
+            maxMovesY = 1;
+            break;
+
+        }
+    }
+    void movePiece(){
+        board[rowInt+maxMovesX][(column-1) + maxMovesY];
+        // calculate all the moves that can be done and add them to the move vector, then show the player these moves
+
+    }
+public:
+    void beginGame(){
+        bool end = true;
+        std::cout << "Enter you choice of piece in the following way\nLetter then number e.g B1, C4, H8\n";
+        while(end == true){
+            printBoard();
+            std::cout << "Choose Row and Column: ";
+            std::getline(std::cin, input);
+            if(checkAndBreak(input) == false){
+                std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+                continue;
+            }
+            getSelection();
+            std::cout << "You have selected the " << pieces[selection] << "\n";
+            std::cout << "This has a maximum X move of " << maxMovesX << " and a maximum Y move of " << maxMovesY << "\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+        }
+
+    }
 
 
     chess();
     ~chess();
 };
 
+
+
+
+
+
+
+
+
+
+
+//Hey I thought you looked cute in your profile picture so I decided to send you a friend request. Add me if you want to discuss trading lego instructions because I got some pretty rare ones baby
 #endif // CHESS_H
--- chess/main.cpp
+++ chess/main.cpp
@@ -1,24 +1,8 @@
 
 #include <memory>
 #include <chess.h>
-
-
-chess::chess(){
-    bool end = true;
-    while(end == true){
-            std::cout << "Choose Row ";
-            std::cin >> row;
-            for(int i = 0; i < alpha.size(); i++){
-                    if(row == alpha[i]){
-                        break;
-                    }
-            std::cout << "Input not accepted\n";
-        }
-    }
-}
-chess::~chess(){
-
-}
+chess::chess(){}
+chess::~chess(){}
 
 
 
@@ -28,52 +12,8 @@ chess::~chess(){
 int main(){
 
     chess* Chess = new chess;
+    Chess->beginGame();
 
-    // Chess->printBoard();
-
-    /*
-    std::cout << "Choose Row: ";
-    std::cin >> row;
-    for(int i = 0; i < 8; i++){
-
-    }
-    std::cout << "Choose Column: ";
-    std::cin >> column;
-
-    switch(row){
-    case 'A':
-        rowInt = 0;
-        break;
-    case 'B':
-        rowInt = 1;
-        break;
-    case 'C':
-        rowInt = 2;
-        break;
-    case 'D':
-        rowInt = 3;
-        break;
-    case 'E':
-        rowInt = 4;
-        break;
-    case 'F':
-        rowInt = 5;
-        break;
-    case 'G':
-        rowInt = 6;
-        break;
-    case 'H':
-        rowInt = 7;
-        break;
-    default:
-        std::cout << "Letter is not Valid\n";
-        break;
-    }
-
-    char selection = a[rowInt][column-1];
-
-    std::cout << selection << "\n";
-    */
     delete Chess;
     return 0;
 }
