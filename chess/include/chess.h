92992357fee9596be279f9f2efaca2c90f2772db
LtMax2102 <selwynj@coventry.ac.uk>
2022-04-25 03:00:31 +0100

    hi

--- chess/include/chess.h
+++ chess/include/chess.h
@@ -12,7 +12,7 @@ class chess {
     int count = 0, column, letter = 1, rowInt, maxMovesY, maxMovesX, movement;
     char  row, buffer;
     char* selection;
-    std::string input;
+    std::string input, placeHolder;
     std::map<char, std::string> pieces = {{'R', "Rook"}, {'H', "Horse"},{'B', "Bishop"},{'Q', "Queen"},{'K', "King"},{'P', "Pawn"}};
     std::vector<std::string> validMoves;
     std::array<char,8> alpha = {'A','B','C','D','E','F','G','H'};
@@ -49,7 +49,7 @@ class chess {
         std::cout << std::endl;
     }
     bool checker(char &c){
-        for(int i = 0; i < alpha.size(); i++){
+        for(unsigned i = 0; i < alpha.size(); i++){
             if(c == alpha[i]){
                 return true;
             }
@@ -127,14 +127,30 @@ class chess {
         if(*selection == '-'){
             return;
         }
+
+
         std::cout << "You have selected the " << pieces[*selection] << "\n";
         std::cout << "This has a maximum X move of " << maxMovesX << " and a maximum Y move of " << maxMovesY << "\n";
         buffer = board[rowInt+maxMovesY][(column-1) + maxMovesX];
         if(buffer != '-'){
             return;
         }
-        board[rowInt+maxMovesY][(column-1) + maxMovesX] = *selection;
-        *selection = buffer;
+        for(int i = 0; i <=  maxMovesY; i++){
+            if(board[rowInt+i][column-1] == '-'){
+                placeHolder.push_back(alpha[rowInt+i]);
+//                placeHolder.insert()
+                validMoves.push_back(placeHolder);
+            }
+            break;
+        }
+        std::cout << "Printing moves: \n";
+        for(const auto& i : validMoves){
+            std::cout << i << " ";
+        }
+        std::cout << "\n";
+
+//        board[rowInt+maxMovesY][(column-1) + maxMovesX] = *selection;
+//        *selection = buffer;
 
         // calculate all the moves that can be done and add them to the move vector, then show the player these moves
 
--- chess/main.cpp
+++ chess/main.cpp
@@ -14,6 +14,7 @@ int main(){
     chess* Chess = new chess;
     Chess->beginGame();
 
+
     delete Chess;
     return 0;
 }
